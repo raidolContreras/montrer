6b977c0f039e796d257c155863e8efed6a29c844
raidolContreras <oscarcontrerasf91@gmail.com>
2023-12-19 15:26:35 -0600

    sidenav

--- view/pages/navs/sidenav.php
+++ view/pages/navs/sidenav.php
@@ -1,17 +1,23 @@
 <!-- Start Sidebar Menu Area -->
 <nav class="sidebar-menu">
-	<ul class="list-group flex-column d-inline-block first-menu" data-simplebar>
-		<li class="list-group-item main-grid active">
-			<a href="inicio" class="icon">
-				<img src="assets/img/svg/element.svg" alt="element">
-			</a>
-		</li>
+    <ul class="list-group flex-column d-inline-block first-menu" data-simplebar>
+        <?php 
+        	$active = (!isset($_GET['pagina']) || $_GET['pagina'] == 'inicio') ? 'active' : '';
+        ?>
+        <li class="list-group-item main-grid <?php echo $active; ?>">
+            <a href="inicio" class="icon">
+                <img src="assets/img/svg/element.svg" alt="element">
+            </a>
+        </li>
 
-		<li class="list-group-item main-grid">
-			<a href="registers" class="icon">
-				<img src="assets/img/svg/profile-2user.svg" alt="calendar">
-			</a>
-		</li>
-	</ul>
+        <?php 
+        	$active = (isset($_GET['pagina']) && ($_GET['pagina'] == 'registers' || $_GET['pagina'] == 'register')) ? 'active' : '';
+        ?>
+        <li class="list-group-item main-grid <?php echo $active; ?>">
+            <a href="registers" class="icon">
+                <img src="assets/img/svg/profile-2user.svg" alt="calendar">
+            </a>
+        </li>
+    </ul>
 </nav>
 <!-- End Sidebar Menu Area -->
