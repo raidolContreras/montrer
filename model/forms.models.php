86b06603237feff0035a3e3c71e37f90960673fa
raidolContreras <oscarcontrerasf91@gmail.com>
2023-12-19 15:29:24 -0600

    acomodo

--- model/forms.models.php
+++ model/forms.models.php
@@ -186,17 +186,17 @@ class FormsModels {
 	
 	static public function mdlGetUsers(){
 		$pdo = Conexion::conectar();
-    $sql = "SELECT * FROM montrer_users";
-    $stmt = $pdo->prepare($sql);
-    
-    if ($stmt->execute()){
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } else {
-        print_r($pdo->errorInfo());
-    }
-    
-    // Asegúrate de cerrar la conexión en el bloque finally
-    $stmt->closeCursor();
-    $stmt = null;
+		$sql = "SELECT * FROM montrer_users";
+		$stmt = $pdo->prepare($sql);
+		
+		if ($stmt->execute()){
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		} else {
+			print_r($pdo->errorInfo());
+		}
+		
+		// Asegúrate de cerrar la conexión en el bloque finally
+		$stmt->closeCursor();
+		$stmt = null;
 	}
 }
