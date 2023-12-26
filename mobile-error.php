fbbd1cf6899945b5bca4e5cffd8e3f0facf02f08
raidolContreras <oscarcontrerasf91@gmail.com>
2023-12-26 09:20:08 -0600

    mobile-error.php

---
+++ mobile-error.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <!-- Required meta tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Error - Acceso desde Dispositivo Móvil</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="assets/img/svg/favicon.svg">
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8faff;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #555;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Error - Acceso desde Dispositivo Móvil</h1>
+    <p>Lo sentimos, el acceso a esta página desde dispositivos móviles no está permitido.</p>
+</body>
+
+</html>
