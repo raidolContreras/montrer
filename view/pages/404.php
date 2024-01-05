0dfd4762c8de408a5e017e31c5eb40c7eec55a83
raidolContreras <oscarcontrerasf91@gmail.com>
2024-01-05 09:07:45 -0600

    Update 404.php

--- view/pages/404.php
+++ view/pages/404.php
@@ -3,6 +3,12 @@
 		<div class="d-table-cell">
 			<div class="container">
 				<div class="error-content card-box-style">
+					<div class="account-header">
+						<a href="inicio">
+							<img src="assets/img/logo.png" alt="main-logo">
+						</a>
+						<h3>Registro de nuevos usuarios</h3>
+					</div>
 					<h1>404</h1>
 					<h3>¡Ups! Página no encontrada</h3>
 					<p>La página que buscabas no se pudo encontrar.</p>
