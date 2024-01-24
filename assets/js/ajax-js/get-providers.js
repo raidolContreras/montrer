227813519bfd4346dd1d72bd305245154dd9e58d
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-01-24 02:14:42 -0600

    Update get-providers.js

--- assets/js/ajax-js/get-providers.js
+++ assets/js/ajax-js/get-providers.js
@@ -77,6 +77,125 @@ $(document).ready(function () {
 
 });
 
+    // Manejar el clic del botón de edición
+    $('#provider').on('click', '.edit-button', function() {
+        var idProvider = $(this).data('id');
+        sendForm('editProvider', idProvider);
+    });
+
+    // Manejar el clic del botón de habilitar área
+    $('#provider').on('click', '.enable-button', function () {
+        var idProvider = $(this).data('id');
+        var ProviderName = $(this).closest('tr').find('td:eq(1) a').text(); // Obtener el nombre del área desde la fila
+
+        // Mostrar el nombre del área en el modal
+        $('#enableProviderName').text(ProviderName);
+
+        // Mostrar el modal de habilitar área
+        $('#enableProviderModal').modal('show');
+
+        // Manejar el clic del botón "Habilitar" en el modal
+        $('#confirmEnableProvider').on('click', function () {
+            $.ajax({
+                type: 'POST',
+                url: 'controller/ajax/ajax.form.php', // Ajusta la URL según tu estructura
+                data: { 'enableProvider': idProvider },
+                success: function (response) {
+                    if (response === 'ok') {
+                        // Implementa acciones adicionales si es necesario
+                        console.log('proveedor habilitado con éxito');
+                        providerTable.ajax.reload(); // Recargar datos de DataTable
+                    } else {
+                        console.error('No se pudo habilitar el proveedor');
+                    }
+                },
+                complete: function () {
+                    // Ocultar el modal después de completar la solicitud
+                    idProvider = 0;
+                    $('#enableProviderModal').modal('hide');
+                }
+            });
+        });
+    });
+
+    // Manejar el clic del botón de habilitar área
+    $('#provider').on('click', '.disable-button', function () {
+        var idProvider = $(this).data('id');
+        var ProviderName = $(this).closest('tr').find('td:eq(1) a').text(); // Obtener el nombre del área desde la fila
+
+        // Mostrar el nombre del área en el modal
+        $('#disableProviderName').text(ProviderName);
+
+        // Mostrar el modal de habilitar área
+        $('#disableProviderModal').modal('show');
+
+        // Manejar el clic del botón "Habilitar" en el modal
+        $('#confirmDisableProvider').on('click', function () {
+            $.ajax({
+                type: 'POST',
+                url: 'controller/ajax/ajax.form.php', // Ajusta la URL según tu estructura
+                data: { 'disableProvider': idProvider },
+                success: function (response) {
+                    if (response === 'ok') {
+                        // Implementa acciones adicionales si es necesario
+                        console.log('proveedor deshabilitado con éxito');
+                        providerTable.ajax.reload(); // Recargar datos de DataTable
+                    } else {
+                        console.error('No se pudo deshabilitar el proveedor');
+                    }
+                },
+                complete: function () {
+                    // Ocultar el modal después de completar la solicitud
+                    idProvider = 0;
+                    $('#disableProviderModal').modal('hide');
+                }
+            });
+        });
+    });
+
+    // Manejar el clic del botón de eliminar área
+    $('#provider').on('click', '.delete-button', function () {
+        var idProvider = $(this).data('id');
+        var ProviderName = $(this).closest('tr').find('td:eq(1) a').text(); // Obtener el nombre del área desde la fila
+
+        // Mostrar el nombre del área en el modal
+        $('#deleteProviderName').text(ProviderName);
+
+        // Mostrar el modal de eliminar área
+        $('#deleteProviderModal').modal('show');
+
+        // Manejar el clic del botón "eliminar" en el modal
+        $('#confirmDeleteProvider').on('click', function () {
+            $.ajax({
+                type: 'POST',
+                url: 'controller/ajax/ajax.form.php', // Ajusta la URL según tu estructura
+                data: { 'deleteProvider': idProvider },
+                success: function (response) {
+                    if (response === 'ok') {
+                        location.reload();
+                    } else {
+                        console.error('No se pudo eliminar el proveedor');
+                    }
+                },
+                complete: function () {
+                    // Ocultar el modal después de completar la solicitud
+                    idProvider = 0;
+                    $('#deleteProviderModal').modal('hide');
+                }
+            });
+        });
+    });
+
+    function sendForm(action, idProvider) {
+        // Crear un formulario oculto y agregar el idProvider como un campo oculto
+        var form = $('<form action="' + action + '" method="post"></form>');
+        form.append('<input type="hidden" name="register" value="' + idProvider + '">');
+
+        // Adjuntar el formulario al cuerpo del documento y enviarlo
+        $('body').append(form);
+        form.submit();
+    }
+
 function renderActionButtons(idProvider, status) {
 
     if (status == 1) {
