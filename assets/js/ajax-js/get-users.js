acb119d46d4f3cd77dc0d2190a87a3c7d4049016
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-01-26 13:57:51 -0600

    last bugs to action buttos

--- assets/js/ajax-js/get-users.js
+++ assets/js/ajax-js/get-users.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             },
             { data: 'email' },
             {
-                data: 'nameArea',
+                data: 'nameUser',
                 render: function(data) {
                     if (data == null) {
                         return 'Sin departamento asignado';
@@ -76,180 +76,6 @@ $(document).ready(function () {
         sendForm('editRegister', idUser);
     });
 
-    $('#registers').on('click', '.disable-button', function() {
-        var idUser = $(this).data('id');
-        var userName = $(this).closest('tr').find('td:eq(1)').text(); // Obtener el nombre del usuario desde la fila
-    
-        // Mostrar el nombre del usuario en el modal
-        $('#userName').text(userName);
-    
-        // Mostrar el modal de Deshabilitar
-        $('#disableModal').modal('show');
-    
-        // Manejar el clic del botón "Deshabilitar" en el modal
-        $('#confirmDisable').on('click', function() {
-            handleUserAction(idUser, 'disableUser', 'El usuario ha sido deshabilitado', 'No se pudo deshabilitar al usuario');
-        });
-    });
-    
-    $('#registers').on('click', '.enable-button', function() {
-        var idUser = $(this).data('id');
-        var userName = $(this).closest('tr').find('td:eq(1)').text(); // Obtener el nombre del usuario desde la fila
-    
-        // Mostrar el nombre del usuario en el modal
-        $('#enableUserName').text(userName);
-    
-        // Mostrar el modal de habilitar
-        $('#enableModal').modal('show');
-    
-        // Manejar el clic del botón "Habilitar" en el modal
-        $('#confirmEnable').on('click', function() {
-            handleUserAction(idUser, 'enableUser', 'El usuario ha sido habilitado', 'No se pudo habilitar al usuario');
-        });
-    });
-    
-    $('#registers').on('click', '.delete-button', function() {
-        var idUser = $(this).data('id');
-        var userName = $(this).closest('tr').find('td:eq(1)').text(); // Obtener el nombre del usuario desde la fila
-    
-        // Mostrar el nombre del usuario en el modal
-        $('#deleteUserName').text(userName);
-    
-        // Mostrar el modal de eliminar
-        $('#deleteModal').modal('show');
-    
-        // Manejar el clic del botón "Eliminar" en el modal
-        $('#confirmDelete').on('click', function() {
-            handleUserAction(idUser, 'deleteUser', 'El usuario ha sido eliminado', 'No se pudo eliminar al usuario', 'El usuario pertenece a un departamento');
-        });
-    });
-
-    $('#registers').on('click', '.change-password-button', function() {
-        var idUser = $(this).data('id');
-        var userName = $(this).closest('tr').find('td:eq(1)').text(); // Obtener el nombre del usuario desde la fila
-        
-        // Mostrar el nombre del usuario en el modal
-        $('#changePasswordUserName').text(userName);
-        
-        // Mostrar el modal de cambiar contraseña
-        $('#changePasswordModal').modal('show');
-        
-        // Manejar el clic del botón "Cambiar Contraseña" en el modal
-        $('#confirmChangePassword').on('click', function() {
-            var newPassword = $("input[name='newPassword']").val();
-            var confirmPassword = $("input[name='confirmPassword']").val();
-            changePassword(idUser, newPassword, confirmPassword);
-        });
-    });
-    
-    function handleUserAction(idUser, action, successMessage, errorMessage, errorText) {
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timerProgressBar: false,
-            didOpen: (toast) => {
-                toast.onmouseenter = Swal.stopTimer;
-                toast.onmouseleave = Swal.resumeTimer;
-            }
-        });
-    
-        $.ajax({
-            type: 'POST',
-            url: 'controller/ajax/ajax.form.php', // Ajusta la URL según tu estructura
-            data: { [action]: idUser },
-            success: function (response) {
-                if (response === 'ok'){
-                    Swal.fire({
-                        icon: "success",
-                        title: successMessage
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            location.reload();
-                        }
-                    });
-    
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: errorMessage,
-                        text: errorText
-                    });
-                }
-            },
-            complete: function () {
-                // Ocultar el modal después de completar la solicitud
-                idUser = 0;
-                $('#disableModal, #enableModal, #deleteModal').modal('hide');
-            }
-        });
-    }
-
-    function changePassword(idUser, newPassword, confirmPassword) {
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timerProgressBar: false,
-            didOpen: (toast) => {
-                toast.onmouseenter = Swal.stopTimer;
-                toast.onmouseleave = Swal.resumeTimer;
-            }
-        });
-
-        var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[\w\W]{10,}$/;
-        if (!passwordRegex.test(newPassword)) {
-            console.log(passwordRegex.test('Paquelaquieres1+'));
-
-            // Mostrar un mensaje de error
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'La contraseña debe contener 10 caracteres, de los cuáles obligatoriamente: 1 letra mayúscula, 1 letra minúscula, 1 número y 1 símbolo.',
-            });
-            return;
-        } else if (newPassword !== confirmPassword) {
-            // Mostrar un mensaje de error
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'Las contraseñas no coinciden.',
-            });
-            return;
-        } else {
-            $.ajax({
-                type: 'POST',
-                url: 'controller/ajax/ajax.form.php', // Ajusta la URL según tu estructura
-                data: { idUsers: idUser, newPassword: newPassword },
-                success: function (response) {
-                    if (response === 'ok'){
-                        Swal.fire({
-                            icon: "success",
-                            title: 'Contraseña actualizada con exito'
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-                                location.reload();
-                            }
-                        });
-        
-                    } else {
-                        Swal.fire({
-                            icon: "error",
-                            title: 'Error',
-                            text: 'Nose pudo actualizar la contraseña'
-                        });
-                    }
-                },
-                complete: function () {
-                    // Ocultar el modal después de completar la solicitud
-                    idUser = 0;
-                    $('#changePasswordModal').modal('hide');
-                }
-            });
-        }
-    
-    }
-
     function sendForm(action, idUser) {
         // Crear un formulario oculto y agregar el idUser como un campo oculto
         var form = $('<form action="' + action + '" method="post"></form>');
@@ -297,6 +123,125 @@ $(document).ready(function () {
             `;
         }
     }
+    
+    function showModalAndSetData(modalId, nameId, confirmButtonId, actionType, successMessage) {
+        $('#registers').on('click', `.${actionType}-button`, function () {
+            var idUser = $(this).data('id');
+            var UserName = $(this).closest('tr').find('td:eq(1)').text();
+    
+            $(`#${nameId}`).text(UserName);
+            $(`#${confirmButtonId}`).data('id', idUser);
+    
+            $(`#${modalId}`).modal('show');
+        });
+    
+        $(`#${confirmButtonId}`).off('click').on('click', function () {
+            var idUser = $(this).data('id');
+    
+            $.ajax({
+                type: 'POST',
+                url: 'controller/ajax/ajax.form.php',
+                data: { [`${actionType}User`]: idUser },
+                success: function (response) {
+                    handleResponse(response, actionType, successMessage);
+                },
+                complete: function () {
+                    idUser = 0;
+                    $(`#${modalId}`).modal('hide');
+                }
+            });
+        });
+    }
+    
+    function handleResponse(response, actionType, successMessage) {
+        if (response === 'ok') {
+            Swal.fire({
+                icon: "success",
+                title: `Departamento ${successMessage} con éxito`
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    location.reload();
+                }
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: 'Error',
+                text: `No se pudo ${actionType.toLowerCase()} el departamento`
+            });
+        }
+    }
+    
+    showModalAndSetData('disableModal', 'disableUserName', 'confirmDisable', 'disable', 'deshabilitado');
+    showModalAndSetData('enableModal', 'enableUserName', 'confirmEnable', 'enable', 'habilitado');
+    showModalAndSetData('deleteModal', 'deleteUserName', 'confirmDelete', 'delete', 'eliminado');
+
+    function showModalAndSetData(modalId, nameId, confirmButtonId, action) {
+        $('#registers').on('click', `.${action}-button`, function () {
+            var idUser = $(this).data('id');
+            var userName = $(this).closest('tr').find('td:eq(1)').text();
+    
+            $(`#${nameId}`).text(userName);
+            $(`#${modalId}`).modal('show');
+    
+            $(`#${confirmButtonId}`).off('click').on('click', function () {
+                handlePasswordChange(idUser);
+            });
+        });
+    }
+    
+    function handlePasswordChange(idUser) {
+        var newPassword = $("input[name='newPassword']").val();
+        var confirmPassword = $("input[name='confirmPassword']").val();
+    
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[\w\W]{10,}$/;
+    
+        if (!passwordRegex.test(newPassword)) {
+            showError('La contraseña debe contener 10 caracteres, de los cuales obligatoriamente: 1 letra mayúscula, 1 letra minúscula, 1 número y 1 símbolo.');
+            return;
+        } else if (newPassword !== confirmPassword) {
+            showError('Las contraseñas no coinciden.');
+            return;
+        }
+    
+        $.ajax({
+            type: 'POST',
+            url: 'controller/ajax/ajax.form.php',
+            data: { idUsers: idUser, newPassword: newPassword },
+            success: function (response) {
+                handleResponse(response, 'Contraseña actualizada con éxito');
+            },
+            complete: function () {
+                idUser = 0;
+                $('#changePasswordModal').modal('hide');
+            }
+        });
+    }
+    
+    function showError(message) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: message,
+        });
+    }
+    
+    function handleResponse(response, successMessage) {
+        if (response === 'ok') {
+            Swal.fire({
+                icon: "success",
+                title: successMessage
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    location.reload();
+                }
+            });
+        } else {
+            showError('No se pudo actualizar la contraseña');
+        }
+    }
+    
+    showModalAndSetData('changePasswordModal', 'changePasswordUserName', 'confirmChangePassword', 'change-password');
     
     
 });
