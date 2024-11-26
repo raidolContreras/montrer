cf6f864c11b85a845606cc5f11f94477434f9d3c
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-11-26 12:13:09 -0600

    Update provider.php

--- view/pages/provider/provider.php
+++ view/pages/provider/provider.php
@@ -4,9 +4,9 @@
         <div class="total-browse-content card-box-style single-features">
             <div class="main-title d-flex justify-content-between align-items-center">
                 <h3>Lista de proveedores</h3>
-                <?php if ($level == 1): ?>
+                <?php //if ($level == 1): ?>
                     <a class="btn btn-primary" href="registerProvider">Nuevo proveedor</a>
-                <?php endif ?>
+                <?php //endif ?>
             </div>
 
             <table class="table table-responsive" id="provider">
