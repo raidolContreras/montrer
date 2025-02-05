1bd9b2c9e499e3b8772184be031697836a73da16
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2025-02-04 20:07:40 -0600

    cambio de mensaje

--- view/pages/moduls/home/features.php
+++ view/pages/moduls/home/features.php
@@ -2,9 +2,9 @@
 <div class="features-area">
 	<div class="container-fluid">
 		<div class="row justify-content-center">
-				<?php if ($level == 1): ?>
-					<div class="col-lg-4 col-md-6">
-						<a href="exercise" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ejercicios">
+			<?php if ($level == 1): ?>
+				<div class="col-lg-4 col-md-6">
+					<a href="exercise" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ejercicios">
 						<div class="single-features">
 							<div class="row align-items-center">
 								<div class="col-xl-12">
@@ -16,11 +16,27 @@
 								</div>
 							</div>
 						</div>
-						</a>
-					</div>
+					</a>
+				</div>
 
-					<div class="col-lg-4 col-md-6">
-						<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
+				<div class="col-lg-4 col-md-6">
+					<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
+						<div class="single-features">
+							<div class="row align-items-center">
+								<div class="col-xl-12">
+									<div class="single-click-content">
+										<span class="features-title">Presupuesto total asignado a departamentos</span>
+										<h3 class="total-use"></h3>
+										<p class="budget-message-uses"></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="col-lg-4 col-md-6">
+					<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
 						<div class="single-features">
 							<div class="row align-items-center">
 								<div class="col-xl-12">
@@ -32,11 +48,11 @@
 								</div>
 							</div>
 						</div>
-						</a>
-					</div>
+					</a>
+				</div>
 
-					<div class="col-lg-4 col-md-6">
-						<a href="exercise" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ejercicios">
+				<div class="col-lg-4 col-md-6">
+					<a href="exercise" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ejercicios">
 						<div class="single-features">
 							<div class="row align-items-center">
 								<div class="col-xl-12">
@@ -48,11 +64,11 @@
 								</div>
 							</div>
 						</div>
-						</a>
-					</div>
+					</a>
+				</div>
 
-					<div class="col-lg-6 col-md-6">
-						<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
+				<div class="col-lg-6 col-md-6">
+					<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
 						<div class="single-features">
 							<div class="row align-items-center">
 								<div class="col-xl-12">
@@ -64,11 +80,11 @@
 								</div>
 							</div>
 						</div>
-						</a>
-					</div>
+					</a>
+				</div>
 
-					<div class="col-lg-6 col-md-6">
-						<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
+				<div class="col-lg-6 col-md-6">
+					<a href="budgets" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Asignación de presupuestos">
 						<div class="single-features">
 							<div class="row align-items-center">
 								<div class="col-xl-12">
@@ -80,30 +96,30 @@
 								</div>
 							</div>
 						</div>
-						</a>
-					</div>
-				<?php else: ?>
-					<div class="overview-area">
-						<div class="container-fluid">
-							<div class="overview-content-wrap card-box-style">
-								<div class="row justify-content-center">
-									<div class="overview-content d-flex justify-content-between align-items-center">
+					</a>
+				</div>
+			<?php else: ?>
+				<div class="overview-area">
+					<div class="container-fluid">
+						<div class="overview-content-wrap card-box-style">
+							<div class="row justify-content-center">
+								<div class="overview-content d-flex justify-content-between align-items-center">
 									<div class="overview-title">
 										<h3>Departamentos asignados</h3>
 									</div>
-									</div>
 								</div>
-								<div class="features-area">
-									<div class="container-fluid">
-										<div class="row justify-content-center dashboard">
-										</div>
+							</div>
+							<div class="features-area">
+								<div class="container-fluid">
+									<div class="row justify-content-center dashboard">
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				<?php endif ?>
-			</div>
+				</div>
+			<?php endif ?>
 		</div>
 	</div>
-	<!-- End Features Area -->
+</div>
+<!-- End Features Area -->
